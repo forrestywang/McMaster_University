17b494e2c0a110429a9a74c54f50e3e56d913bea
Forrest Wang <95055762+forrestywang@users.noreply.github.com>
2023-06-08 17:31:49 -0400

    Uploaded files from laptop

---
+++ MATH_1MP3/Testing/Template.py
@@ -0,0 +1,13 @@
+# @author Forrest Wang
+# [Program Name]
+# [Date]
+#
+# This program is a template used for new Python files.
+
+# Variable(s):
+
+
+# Logic:
+
+
+# Output:
---
+++ MATH_1MP3/Testing/Testing.py
@@ -0,0 +1,21 @@
+# @author Forrest Wang
+# Testing.py
+#
+# This program is used for testing Python code.
+
+# Import(s):
+import numpy as np
+
+
+# Function(s):
+dict = {1: "one", 2: "two", 3: "two"}
+arrayasdf = dict.values()
+print(arrayasdf)
+
+# Variable(s):
+
+
+# Logic:
+
+
+# Output:
